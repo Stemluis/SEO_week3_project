47fdfae061f72d590cc8ec8cbfbc0340ed16b10c
Luis Pagan <stemluis@gmail.com>
2022-07-15 15:06:03 +0000

    Added basic test file

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,8 @@
-import unittest, sys
+import unittest, sys, os
 
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 sys.path.append('../SEO_week3_project') # imports python file from parent directory
 from main import app #imports flask app object
 
@@ -16,6 +19,15 @@ class BasicTests(unittest.TestCase):
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+    
+    def test_new_page(self):
+        response = self.app.get('/new', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_page(self):
+        response = self.app.get('/add', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
 
 if __name__ == "__main__":
     unittest.main()
